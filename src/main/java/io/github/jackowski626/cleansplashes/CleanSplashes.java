2e841f01decdfa75a2918c73223b7317e6041b9a
DbpGaming <LbpDbpGaming@gmail.com>
2022-02-23 14:11:54 -0600

    port to 1.18.1, cleanup, jij

--- src/main/java/io/github/jackowski626/cleansplashes/CleanSplashes.java
+++ src/main/java/io/github/jackowski626/cleansplashes/CleanSplashes.java
@@ -1,15 +1,16 @@
 package io.github.jackowski626.cleansplashes;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import io.github.jackowski626.splashesapi.SplashesAPI;
 import net.fabricmc.api.ModInitializer;
 
 public class CleanSplashes implements ModInitializer {
     @Override
     public void onInitialize() {
+		String[] splashesToAdd = {"Notch <3 ez!", "Made by Notch!", "The Work of Notch!", "110813!", "Sexy!"};
+		for (String s : splashesToAdd) {
+			SplashesAPI.addSplash(s);
+		}
+		
 		String[] splashesToRemove = {"Black lives matter!", "Be anti-racist!","Learn about allyship!", "Speak OUT against injustice and UP for equality!", "Amplify and listen to BIPOC voices!", "Educate your friends on anti-racism!", "Support the BIPOC community and creators!", "Stand up for equality in your community!"};
 		for (String s : splashesToRemove) {
 			SplashesAPI.removeSplash(s);
